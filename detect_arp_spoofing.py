98167da3dbf105c58f36658dd072855212bf9675
vboxuser <vboxuser@technoz.myguest.virtualbox.org>
2023-05-08 23:08:41 +0300

    arp

--- detect_arp_spoofing.py
+++ detect_arp_spoofing.py
@@ -1,13 +1,17 @@
 from scapy.all import *
 from scapy.layers.l2 import ARP, Ether
 
-
+mac_to_ip = {}
 def arp_display(pkt):
     if ARP in pkt and pkt[ARP].op == 2:  # ARP reply
         if pkt[ARP].psrc == pkt[ARP].pdst:
             return  # ignore gratuitous ARP
-        if pkt[ARP].hwsrc != pkt[Ether].src:
-            print(f"ARP spoofing detected! Attacker IP: {pkt.src}")
+        if pkt[ARP].hwsrc in mac_to_ip != pkt[Ether].src:
+            if mac_to_ip[pkt[ARP].hwsrc] != pkt[IP].src:
+            	print(f"ARP spoofing detected! Attacker IP: {pkt.src}")
+        else:
+            mac_to_ip[pkt[ARP].hwsrc] = pkt[IP].src
+	   	
 
 
 sniff(prn=arp_display, filter="arp", store=0)
