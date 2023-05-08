7562047c75113f3cf8eece35dfdf3bb0abf3dbc2
amit1153 <amit.harel.043@gmail.com>
2023-05-08 23:20:06 +0300

    arp

--- detect_arp_spoofing.py
+++ detect_arp_spoofing.py
@@ -1,13 +1,16 @@
 from scapy.all import *
-from scapy.layers.l2 import ARP, Ether
+from scapy.layers.l2 import ARP
+
+ip_mac_pairs = {}
 
 
 def arp_display(pkt):
-    if ARP in pkt and pkt[ARP].op == 2:  # ARP reply
-        if pkt[ARP].psrc == pkt[ARP].pdst:
-            return  # ignore gratuitous ARP
-        if pkt[ARP].hwsrc != pkt[Ether].src:
-            print(f"ARP spoofing detected! Attacker IP: {pkt.src}")
+    if pkt[ARP].op == 1:  # If this is an ARP request
+        if pkt[ARP].psrc in ip_mac_pairs and ip_mac_pairs[pkt[ARP].psrc] != pkt[ARP].hwsrc:
+            # If the IP address is already in our dictionary and the MAC address doesn't match
+            print("Possible ARP spoofing detected: IP: {} MAC: {}".format(pkt[ARP].psrc, pkt[ARP].hwsrc))
+        else:
+            ip_mac_pairs[pkt[ARP].psrc] = pkt[ARP].hwsrc  # Add the IP/MAC pair to our dictionary
 
 
 sniff(prn=arp_display, filter="arp", store=0)
