b01225def3543d67f72d49fcea55935b84655dee
amit1153 <amit.harel.043@gmail.com>
2023-05-08 23:20:57 +0300

    arp

--- detect_arp_spoofing.py
+++ detect_arp_spoofing.py
@@ -3,7 +3,7 @@ from scapy.layers.l2 import ARP
 
 ip_mac_pairs = {}
 
-
+mac_to_ip = {}
 def arp_display(pkt):
     if pkt[ARP].op == 1:  # If this is an ARP request
         if pkt[ARP].psrc in ip_mac_pairs and ip_mac_pairs[pkt[ARP].psrc] != pkt[ARP].hwsrc:
@@ -11,6 +11,7 @@ def arp_display(pkt):
             print("Possible ARP spoofing detected: IP: {} MAC: {}".format(pkt[ARP].psrc, pkt[ARP].hwsrc))
         else:
             ip_mac_pairs[pkt[ARP].psrc] = pkt[ARP].hwsrc  # Add the IP/MAC pair to our dictionary
+
 
 
 sniff(prn=arp_display, filter="arp", store=0)
